faeeceae09b3d98973fb9851d6baa934ba57a60a
SDemonUA <sokoldv@ukr.net>
2012-12-30 23:35:28 +0200

    Added regex search for TODO comments

--- todo.py
+++ todo.py
@@ -1,46 +1,82 @@
-import sublime, sublime_plugin
+import sublime, sublime_plugin, re
+
+def get_comment_prefixes(view, pt):
+    shell_vars = view.meta_info("shellVariables", pt)
+    if not shell_vars:
+        return []
+
+    # transform the list of dicts into a single dict
+    all_vars = {}
+    for v in shell_vars:
+        if 'name' in v and 'value' in v:
+            all_vars[v['name']] = v['value']
+
+    prefixes = []
+
+    # transform the dict into a single array of valid comments
+    suffixes = [""] + ["_" + str(i) for i in xrange(1, 10)]
+    for suffix in suffixes:
+        start = all_vars.setdefault("TM_COMMENT_START" + suffix)
+
+        if start:
+            prefixes.append(start)
+            prefixes.append(start.strip())
+
+    return prefixes
 
 class ShowTodoCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		todo_selector = 'TODO:'
-		self.todos = []
-		ts = []
+    def run(self, edit):
+        s = sublime.load_settings('Todo.sublime-settings')
+        todo_selectors = []
+        self.todos = []
+        todos = []
+        ts = []
 
-		v = self.view
-		todos = []
-		# Split comments into lines
-		for r in v.find_by_selector('comment'):
-			todos += v.lines(r);
+        for regex in s.get('todo_string_prefix', ["^TODO"]):
+            todo_selectors.append(re.compile(regex, re.I))
 
-		# Filter comments by TODO selector
-		for r in todos:
-			s = v.substr(r)
-			idx = s.find(todo_selector, 0, 10)
-			if idx is not -1:
-				ts.append(s[idx+len(todo_selector):30].lstrip())
-				self.todos.append(r)
+        # Split comments into lines
+        v = self.view
+        for r in v.find_by_selector('comment'):
+            todos += v.lines(r) # list + list
 
-		# Show panel with TODOs
-		if len(ts) > 0:
-			# v = self.window.get_output_panel('todo_list')
+        # Filter comments by TODO selector
+        for r in todos:
+            comment_prefixes = get_comment_prefixes(v, r.begin())
+            s = v.substr(r).strip()
+            for pref in comment_prefixes:
+                if s.find(pref, 0) is not -1:
+                    s = s[len(pref):len(s)].lstrip()
+                    for regex in todo_selectors:
+                        if regex.match(s):
+                            ts.append(s)
+                            self.todos.append(r)
+                            break
+                    break
 
-			# edit = v.begin_edit()
-			# v.insert(edit, 0, u"\n".join(line for line in ts))
-			# v.end_edit(edit)
+        # Show panel with TODOs
+        if len(ts) > 0:
+            v.window().show_quick_panel(ts, self.on_done, sublime.MONOSPACE_FONT)
+            # v = self.window.get_output_panel('todo_list')
 
-			# self.window.run_command("show_panel", {"panel": "output.todo_list"})
-			v.window().show_quick_panel(ts, self.on_done)
-		else:
-			sublime.status_message("No TODOs in this file.")
-		return
+            # edit = v.begin_edit()
+            # v.insert(edit, 0, u"\n".join(line for line in ts))
+            # v.end_edit(edit)
 
-	def on_done(self, idx):
-		if idx is not -1:
-			self.view.show(self.todos[idx])
-		return
+            # self.window.run_command("show_panel", {"panel": "output.todo_list"})
+        else:
+            sublime.status_message("No TODOs in this file.")
+        return
 
-	def is_visible(args):
-		return True
+    def on_done(self, idx):
+        if idx is not -1:
+            self.view.sel().clear()
+            self.view.sel().add(self.todos[idx])
+            self.view.show(self.todos[idx])
+        return
 
-	def description():
-		return "See TODOs in openned file."
+    def is_visible(args):
+        return True
+
+    def description(args):
+        return "List TODOs in openned file."
